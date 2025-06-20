88dd16277baf06da5eda91ef89441dad44cc027f
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 13:27:16 +0000

    [automatic commit] 2025-06-20 13:27:15 UTC 58.174.18.53

--- part1/app.js
+++ part1/app.js
@@ -71,7 +71,10 @@ let db;
         INSERT INTO Dogs (owner_id, name, size)
         VALUES
         ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
-        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small')
+        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+        ((SELECT user_id FROM Users WHERE username = 'ninaowner'), 'Ziggy', 'large'),
+        ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Luna', 'small'),
+        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Otis', 'medium')
       `);
     }
 
