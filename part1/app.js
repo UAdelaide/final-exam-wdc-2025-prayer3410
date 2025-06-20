b5dbfa70b6e326a3b7cf20b8ac6cae5c2500aaea
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 13:10:47 +0000

    [automatic commit] 2025-06-20 13:10:46 UTC 58.174.18.53

--- part1/app.js
+++ part1/app.js
@@ -10,6 +10,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
