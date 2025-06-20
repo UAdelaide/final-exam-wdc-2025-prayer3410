88b72571c15853d2e01213342797ea3a800c518f
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 13:26:08 +0000

    [automatic commit] 2025-06-20 13:26:07 UTC 58.174.18.53

--- part1/app.js
+++ part1/app.js
@@ -58,10 +58,8 @@ let db;
       await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
         ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-        ('lucaswalker', 'lucas@walkers.net', 'hashedabc', 'walker'),
-        ('ninaowner', 'nina@doglovers.org', 'hashednina', 'owner')
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
       `);
     }
 
