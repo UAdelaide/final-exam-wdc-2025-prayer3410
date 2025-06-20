15b43ce4f0fdd66ab8306bbd1e7280ec41f46df4
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 13:18:37 +0000

    [automatic commit] 2025-06-20 13:18:37 UTC 58.174.18.53

--- part1/app.js
+++ part1/app.js
@@ -92,7 +92,7 @@ app.get('/api/dogs', async (req, res) => {
     res.json(rows);
   } catch (err) {
     console.error('Error fetching /api/dogs:', err);
-    res.status(500).json({ error: 'Failed to retrieve dogs' });
+    res.status(500).json({ error: 'Failed to retrieve' });
   }
 });
 
